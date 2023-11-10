4fccab704febf704469fe420500927bdeb7957a1
Alex12345005 <rath190134@sr.htlweiz.at>
2023-11-10 15:44:13 +0100

    added new model/schema& updated functions not work

--- pms/models.py
+++ pms/models.py
@@ -1,10 +1,11 @@
 import datetime
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, TIMESTAMP
 from sqlalchemy.orm import relationship
 
 from .database import Base
 
 class LoginCredentials(Base):
+    """Model representing the login credentials."""
     __tablename__ = 'login_credentials'
 
     id = Column(Integer, primary_key=True, unique=True)
@@ -12,11 +13,37 @@ class LoginCredentials(Base):
     password = Column(String(40))
     is_active = Column(Boolean, default=True)
 
-class VehicleInfo(Base):
-    __tablename__ = 'vehicle_info'
+    class Config:
+        arbitrary_types_allowed = True
 
-    id = Column(Integer, primary_key=True)
-    license_plate = Column(String(10))
-    name = Column(String(30))
-    parking_time = Column(String(40))
-    created_at = Column(DateTime, default=datetime.datetime.now)
+class Tag(Base):
+    """Model representing a tag for vehicles."""
+    __tablename__ = 'tags'
+
+    tag_id = Column(Integer, primary_key=True)
+    tag_name = Column(String(30), unique=True)
+
+    # Add a one-to-one relationship with Vehicle
+    vehicle = relationship('Vehicle', uselist=False, back_populates='tag')
+
+class ParkingPermission(Base):
+    """Model representing parking permissions for vehicles."""
+    __tablename__ = 'parking_permission'
+
+    parking_permission_id = Column(Integer, primary_key=True)
+    start_time = Column(TIMESTAMP, default=datetime.datetime.now)
+    end_time = Column(TIMESTAMP)
+
+class Vehicle(Base):
+    """Model representing a vehicle and its associated information."""
+    __tablename__ = 'vehicles'
+
+    vehicle_id = Column(Integer, primary_key=True)
+    owner_name = Column(String(30))
+    license_plate = Column(String(10), unique=True)
+
+    tag_id = Column(Integer, ForeignKey('tags.tag_id'), unique=True)  # Make it a foreign key
+    tag = relationship('Tag', back_populates='vehicle')
+
+    parking_permission_id = Column(Integer, ForeignKey('parking_permission.parking_permission_id'))
+    parking_permission = relationship('ParkingPermission', back_populates='vehicles')
--- pms/schemas.py
+++ pms/schemas.py
@@ -1,37 +1,92 @@
 import datetime
 from pydantic import BaseModel
 from typing import List
+from .models import *
+from datetime import datetime as dt
 
-#Define schema for Login Credentials
+
+
+# Define schema for Vehicle
+class VehicleBase(BaseModel):
+    owner_name: str
+    license_plate: str
+    tag_id: int
+    parking_permission_id: int
+
+class VehicleCreate(BaseModel):
+    owner_name: str
+    license_plate: str
+
+class Vehicle(BaseModel):
+    vehicle_id: int
+    owner_name: str
+    license_plate: str
+    tag_id: int
+    parking_permission_id: int
+    created_at: dt
+    updated_at: dt
+
+    class Config:
+        """Configuration for Vehicle schema."""
+        from_attributes = True
+        arbitrary_types_allowed = True
+
+# Define schema for Login Credentials
 class LoginCredentialsBase(BaseModel):
+    """Base schema for creating login credentials."""
     username: str
     password: str
 
 class LoginCredentialsCreate(LoginCredentialsBase):
+    """Schema for creating login credentials."""
     username: str
     password: str
 
 class LoginCredentials(LoginCredentialsBase):
+    """Schema for reading login credentials."""
     id: int
     is_active: bool
 
     class Config:
+        """Configuration for LoginCredentials schema."""
         from_attributes = True
+        arbitrary_types_allowed = True
 
-#Define schema for Vehicle Info
-class VehicleInfoBase(BaseModel):
-    license_plate: str
-    name: str
-    parking_time: str
+# Define schema for Tag
+class TagBase(BaseModel):
+    """Base schema for creating a Tag."""
+    tag_name: str
 
-class VehicleInfoCreate(VehicleInfoBase):
-    license_plate: str
-    name: str
-    parking_time: str
+class TagCreate(TagBase):
+    """Schema for creating a Tag."""
+    tag_name: str
 
-class VehicleInfo(VehicleInfoBase):
-    id: int
-    created_at: datetime.datetime
+class Tag(TagBase):
+    """Schema for reading a Tag."""
+    tag_id: int
+    vehicle: Vehicle  # Adjusted to represent one-to-one relationship
 
     class Config:
+        """Configuration for Tag schema."""
         from_attributes = True
+        arbitrary_types_allowed = True
+
+# Define schema for ParkingPermission
+class ParkingPermissionBase(BaseModel):
+    """Base schema for creating a ParkingPermission."""
+    start_time: dt
+    end_time: dt
+
+class ParkingPermissionCreate(ParkingPermissionBase):
+    """Schema for creating a ParkingPermission."""
+    start_time: dt
+    end_time: dt
+
+class ParkingPermission(ParkingPermissionBase):
+    """Schema for reading a ParkingPermission."""
+    parking_permission_id: int
+
+    class Config:
+        """Configuration for ParkingPermission schema."""
+        from_attributes = True
+        arbitrary_types_allowed = True
